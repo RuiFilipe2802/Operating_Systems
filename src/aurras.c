7683a3f2ab4ed98fd07db8658a11d424cc15195e
RuiFilipe2802 <56730143+RuiFilipe2802@users.noreply.github.com>
2021-01-16 14:26:30 +0000

    Add files via upload

--- src/aurras.c
+++ src/aurras.c
@@ -13,9 +13,7 @@ int main(int argc, char *argv[]){
 
     int fd;
     int fd2;
-    char buffer[25] = "Boas, ta tudo?\n";
     char *args[10];
-    fd = open("fifoC_S", O_WRONLY);
     int sizeArgs=0;
     int i;
     int size;
@@ -26,6 +24,7 @@ int main(int argc, char *argv[]){
 
     if(argc == 1){ //     Show usage protocol
         write(1, "./aurras transform input-filename output-filename filter id-1 filter id-2 ...", 78);
+        return 1;
     }
 
     if((strcmp(argv[1],"status") == 0) && argc == 2){
@@ -38,6 +37,7 @@ int main(int argc, char *argv[]){
            perror("Command invalid");
            return 1;
         }
+        fd = open("fifoC_S", O_WRONLY);
         for(i = 0;i < argum;i++){
             arguments[i] = argv[i+2];    
             strcat(str,arguments[i]);
@@ -47,7 +47,6 @@ int main(int argc, char *argv[]){
         write(fd, &argum, sizeof(int));
         write(fd, &size, sizeof(int));
         write(fd, str, sizeof(str));
-        printf("%s", str);
     }
 
     return 0;
--- src/aurrasd.c
+++ src/aurrasd.c
@@ -7,30 +7,86 @@
 #include <string.h>
 #include <signal.h>
 
+#define MAX 50
+
 //    SERVIDOR
 
-int main(int argc, char *argv[]){
 
-  write(1, "BEM VINDO\n", sizeof("BEM VINDO\n"));
-  // CLIENTE -> SERVIDOR
-  mkfifo("fifoC_S", 0666);
-  // SERVIDOR -> CLIENTE
-  mkfifo("fifoS_C", 0666);
+int readln(int fd, char *line, int size){
+  int i=0, n;
+  while(i < size && (n = read(fd, &line[i], 1)) == 1 && line[i] != '\n'){
+    i++;
+  }
+  return i;
+}
+
+
+int main(int argc, char *argv[]){
+  
+
+  char *arrFilters[10][MAX];    // ARRAY WITH FILTERS
+  char *arrExec[10][MAX];       // ARRAY WITH FILTER EXECUTABLES 
+  int arrN[10];                 // ARRAY WITH NUMBER OF FILTERS
+  int filters, exe, num, numFilters;
+  write(1, "WELCOME\n", 9);
+  mkfifo("fifoC_S", 0666);      // CLIENTE -> SERVIDOR
+  mkfifo("fifoS_C", 0666);      // SERVIDOR -> CLIENTE
+
+  char buf[25];
+  int sizeBuf;
+  int nLine = 0;
+  int configFile = open("/home/rui/Desktop/grupo-xx/etc/aurrasd.conf", O_RDONLY);
+  while((sizeBuf = readln(configFile, buf, sizeof(buf))) > 0){
+   
+    if((nLine % 3) == 0){          // FILTER
+      arrFilters[filters][MAX] = malloc(sizeof(buf));
+      strncpy(arrFilters[filters][MAX], buf, sizeBuf);
+      printf("FILTER %d: %s\n",filters, arrFilters[filters][MAX]);
+      filters++;  
+    }
+
+    if((nLine % 3) == 1){           // FILTER EXECUTABLE
+      arrExec[exe][MAX] = malloc(sizeof(buf));
+      strncpy(arrExec[exe][MAX], buf, sizeBuf);
+      printf("EXECUTABLE %d: %s\n",exe, arrExec[exe][MAX]);
+      exe++;   
+    }
+
+    if((nLine % 3) == 2){           // NUMBER OF FILTERS
+      numFilters = atoi(buf);
+      arrN[num] = numFilters;
+      printf("FILTER NUMBER %d: %d\n",num, arrN[num]);
+      num++;  
+    }
+    nLine++;
+  }
+
+
+/*
   int wri = open("logs.txt", O_WRONLY | O_CREAT | O_TRUNC, 0600);
   int rea = open("logs.txt", O_RDONLY, 0600);
   int r;
   int size;
   int args;
-
-  int fd = open("fifoC_S", O_RDONLY);
-  read(fd, &args, sizeof(int));         // GET NUMBER OF ARGUMENTS
-  printf("%d\n", args);
-  read(fd, &size, sizeof(int));         // GET SIZE MESSAGE
-  printf("%d\n", size);
-  char buffer[size];
-  read(fd, buffer, size-1);               // GET MESSAGE
-  write(1, buffer, size);
+  char *argums[100];
+  char *p = NULL;
+  int i = 0;
+  char *inputFile;
 
   
+
+  int fd = open("fifoC_S", O_RDONLY);
+  read(fd, &args, sizeof(int));           // GET NUMBER OF ARGUMENTS
+  read(fd, &size, sizeof(int));           // GET SIZE MESSAGE
+  char buffer[size];
+  read(fd, buffer, size-1);               // GET MESSAGE
+
+  for(p = strtok(buffer, " \n"); p != NULL;p = strtok(NULL, " ")){
+    argums[i] = strdup(p);
+    i++; 
+  }
+  inputFile = argums[1];
+  write(1,inputFile, sizeof(inputFile));
+  */
   return 0;
 }
